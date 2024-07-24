b7b314e964d18bcbee545b4b35290404c5c668b3
Facundo Mengoni <mengonifacundo@gmail.com>
2024-07-23 21:02:19 -0300

    Use LOAD_PATH instead of RUBYLIB

--- ruby/run_locally.zsh
+++ ruby/run_locally.zsh
@@ -1,2 +1,2 @@
 bundle install
-clear && RUBYLIB=ruby/modules:ruby/classes ruby ruby/scripts/generate_cv.rb --path=assets/spanish.json
+clear && ruby ruby/scripts/generate_cv.rb --path=assets/spanish.json
