ed146b92bebcd914d9eed3eaa5563544290992ed
Facundo Mengoni <mengonifacundo@gmail.com>
2024-08-11 13:00:34 -0300

    Run rubocop locally while generating the CVs

--- ruby/run_locally.zsh
+++ ruby/run_locally.zsh
@@ -2,3 +2,13 @@ bundle install
 clear
 ruby ruby/scripts/generate_cv.rb --path=assets/data/spanish.json
 ruby ruby/scripts/generate_cv.rb --path=assets/data/english.yml
+
+rubocop
+if [ "$?" != "0" ]; then
+  echo ""
+  read -p "Do you want to try to automatically fix the issues? (y/n): " fix_issues
+  if [ "$fix_issues" == "y" ]; then
+    clear
+    rubocop -A
+  fi
+fi
