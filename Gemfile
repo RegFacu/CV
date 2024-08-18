c80441ae6eeeb3e00832be8744a3e9e85be01bde
Facundo Mengoni <mengonifacundo@gmail.com>
2024-08-18 10:03:32 -0300

    Added rspec & simplecov gems and added script to run test locally

--- Gemfile
+++ Gemfile
@@ -7,4 +7,7 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 gem 'bundler'
 gem 'prawn'
 gem 'prawn-svg'
+gem 'rspec', require: false
 gem 'rubocop', require: false
+gem 'rubocop-rspec', require: false
+gem 'simplecov', require: false, group: :test
---
+++ ruby/run_tests.zsh
@@ -0,0 +1,14 @@
+bundle install
+clear
+
+rubocop
+if [ "$?" != "0" ]; then
+  echo ""
+  read -p "Do you want to try to automatically fix the issues? (y/n): " fix_issues
+  if [ "$fix_issues" == "y" ]; then
+    clear
+    rubocop -A
+  fi
+fi
+
+rspec --format j -o ruby/tests.json ruby
